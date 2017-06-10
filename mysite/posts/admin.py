2b4fcf2b71cc3be46d4ea684289a952012ea82e8
Ubuntu <ubuntu@ip-172-31-33-150.us-east-2.compute.internal>
2017-06-10 10:29:41 +0000

    created a modelAdmin for posts

--- mysite/posts/admin.py
+++ mysite/posts/admin.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Post
+
 # Register your models here.
+
+# Check for options for ModelAdmin
+# https://docs.djangoproject.com/en/1.11/ref/contrib/admin/#modeladmin-options
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title','updated','timestamp']
+    list_display_links = ['title']
+    list_filter = ['updated','timestamp']
+    search_fields = ['title','content']
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
